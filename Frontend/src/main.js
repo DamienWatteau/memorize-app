7b56787aeace1b4d78f1d5fd5107fe36dc4c0b88
DamienWatteau <damienwatteau@outlook.fr>
2022-11-21 14:21:21 +0100

    [Frontend] install buefy(bulma) and sweetalert2

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -3,14 +3,21 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import {ApiUrl} from "./environments"
+
 import axios from "axios"
 import VueAxios from 'vue-axios'
-console.log(process.env)
 axios.defaults.baseURL=ApiUrl;
+Vue.use(VueAxios, axios)
+
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+Vue.use(Buefy);
+
+import VueSweetalert2 from 'vue-sweetalert2';
+// import 'sweetalert2/dist/sweetalert2.min.css';
+Vue.use(VueSweetalert2);
 
 Vue.config.productionTip = false
-
-Vue.use(VueAxios, axios)
 
 router.beforeEach((to, from, next) => {
   document.title = to.meta.title || 'Learning';
